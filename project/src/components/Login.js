8651813be24dec2aa2dd0811ce0e6e4cfc2a3ec5
TomarPratigya <tomarpratigya@gmail.com>
2019-07-01 07:50:23 +0530

    Added support for Session

--- project/src/components/Login.js
+++ project/src/components/Login.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react'
 import { Form, Row, Col, Button } from 'react-bootstrap'
 import './Login.css'
-import Homepage from './Homepage.js'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Router, Route, Redirect } from 'react-router-dom'
+import EmployeeHomepage from './EmployeeHomepage/EmployeeHomepage.js'
 
-const UserSession = require('../../models/UserSession');
-const User = require('../../models/User');
-
-
-export class Login extends Component {
+class Login extends Component {
     constructor(props) {
         super(props)
     
         this.state = {
+            isAuth: false,
             email: '',
             password: '',
             administrator: false
@@ -51,212 +48,25 @@ export class Login extends Component {
             headers:{
                 'Content-Type':'application/json'
             }
-        }
-    )
-    .then(res => res.json())
-    .then(res => console.log(res))
-    .catch(error => console.error(error));
-    event.preventDefault()
+        })
 
-    }
-  }
-  app.post('/api/account/signup', (req, res, next) => {
-    const { body } = req;
-    const {
-      password
-    } = body;
-    let {
-      email
-    } = body;
-
-    if (!email) {
-      return res.send({
-        success: false,
-        message: 'Error: Email cannot be blank.'
-      });
-    }
-    if (!password) {
-      return res.send({
-        success: false,
-        message: 'Error: Password cannot be blank.'
-      });
+        .then(res => res.json())
+        .then((res) => {
+            user.email = res.email
+            if(user.email) this.setState({isAuth: true})
+        })
+        .catch(error => console.error(error));
+        event.preventDefault()
     }
 
-    email = email.toLowerCase();
-    email = email.trim();
-
-    User.find({
-      email: email
-    }, (err, previousUsers) => {
-      if (err) {
-        return res.send({
-          success: false,
-          message: 'Error: Server error'
-        });
-      } else if (previousUsers.length > 0) {
-        return res.send({
-          success: false,
-          message: 'Error: Account already exist.'
-        });
-      }
-
-      // Saves the new user
-      const newUser = new User();
-
-      newUser.email = email;
-      newUser.password = newUser.generateHash(password);
-      newUser.save((err, user) => {
-        if (err) {
-          return res.send({
-            success: false,
-            message: 'Error: Server error'
-          });
-        }
-        return res.send({
-          success: true,
-          message: 'Signed up'
-        });
-      });
-    });
-
-  });
-
-  app.post('/api/account/signin', (req, res, next) => {
-    const { body } = req;
-    const {
-      password
-    } = body;
-    let {
-      email
-    } = body;
-
-
-    if (!email) {
-      return res.send({
-        success: false,
-        message: 'Error: Email cannot be blank.'
-      });
-    }
-    if (!password) {
-      return res.send({
-        success: false,
-        message: 'Error: Password cannot be blank.'
-      });
-    }
-
-    email = email.toLowerCase();
-    email = email.trim();
-
-    User.find({
-      email: email
-    }, (err, users) => {
-      if (err) {
-        console.log('err 2:', err);
-        return res.send({
-          success: false,
-          message: 'Error: server error'
-        });
-      }
-      if (users.length != 1) {
-        return res.send({
-          success: false,
-          message: 'Error: Invalid'
-        });
-      }
-
-      const user = users[0];
-      if (!user.validPassword(password)) {
-        return res.send({
-          success: false,
-          message: 'Error: Invalid'
-        });
-      }
-
-      // the correct user
-      const userSession = new UserSession();
-      userSession.userId = user._id;
-      userSession.save((err, doc) => {
-        if (err) {
-          console.log(err);
-          return res.send({
-            success: false,
-            message: 'Error: server error'
-          });
-        }
-
-        return res.send({
-          success: true,
-          message: 'Valid sign in',
-          token: doc._id
-        });
-      });
-    });
-  });
-
-  app.get('/api/account/verify', (req, res, next) => {
-    // Get the token
-    const { query } = req;
-    const { token } = query;
-    // token=test
-    // Verifies that the token is one of a kind and it's not deleted.
-
-    UserSession.find({
-      _id: token,
-      isDeleted: false
-    }, (err, sessions) => {
-      if (err) {
-        console.log(err);
-        return res.send({
-          success: false,
-          message: 'Error: Server error'
-        });
-      }
-
-      if (sessions.length != 1) {
-        return res.send({
-          success: false,
-          message: 'Error: Invalid'
-        });
-      } else {
-        return res.send({
-          success: true,
-          message: 'Good'
-        });
-      }
-    });
-  });
-
-  app.get('/api/account/logout', (req, res, next) => {
-    // Get the token
-    const { query } = req;
-    const { token } = query;
-    // token=test
-    // Verifies the token is one of a kind and it's not deleted.
-
-    UserSession.findOneAndUpdate({
-      _id: token,
-      isDeleted: false
-    }, {
-      $set: {
-        isDeleted:true
-      }
-    }, null, (err, sessions) => {
-      if (err) {
-        console.log(err);
-        return res.send({
-          success: false,
-          message: 'Error: Server error'
-        });
-      }
-
-      return res.send({
-        success: true,
-        message: 'Good'
-      });
-    });
-  });
-  
     render() {
+        if(this.state.isAuth) {
+            return (
+                <div>
+                    <Redirect to="/user" />
+                </div>
+            )
+        }
         return (
             <div className="first">
                 <Form onSubmit={this.handleSubmit}>
@@ -300,7 +110,7 @@ export class Login extends Component {
 
                     <Form.Group as={Row}>
                         <Col sm={{ span: 10, offset: 2 }}>
-                            <Button type="submit" >Log in</Button>
+                            <Button type="submit">Log in</Button>
                         </Col>
                     </Form.Group>
                 </Form>
